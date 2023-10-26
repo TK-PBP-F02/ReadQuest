6cb429a07c82811a083001e083e6098a8955b7e5
Hafizmuh18 <hafizmuh18@gmail.com>
2023-10-26 13:29:30 +0700

    User and Landing page

--- books/models.py
+++ books/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 
 class Book(models.Model):
+    isbn = models.CharField(max_length=100)
     title = models.CharField(max_length=255)
-    author = models.CharField(max_length=255)
     description = models.TextField()
-    published_date = models.TextField()
-    thumbnail = models.URLField()
+    author = models.CharField(max_length=255)
+    publisher = models.CharField(max_length=255)
+    publication_date = models.CharField(max_length=100)
+    page_count = models.IntegerField()
+    category = models.CharField(max_length=255)
+    image_url = models.URLField()
+    lang = models.CharField(max_length=100)
     readed = models.IntegerField(default=0)
+    buys = models.IntegerField(default=0)
