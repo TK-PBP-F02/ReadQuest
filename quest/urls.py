142b00fc50097040a417cd738b2578ebe122d422
Hafizmuh18 <hafizmuh18@gmail.com>
2023-12-02 15:47:29 +0700

    Do Quest

--- quest/urls.py
+++ quest/urls.py
@@ -12,4 +12,7 @@ urlpatterns = [
     path('quest/<int:id>/', views.quest_detail, name='quest_detail'),
     path('quest-book-detail/<int:pk>/', views.quest_book_detail, name='quest_book_detail'),
     path('quest/json-all/', views.view_json_quest, name='view_json_quest'),
+    path('quest/json-world/', views.view_json_quest_world, name='view_json_quest_world'),
+    path('quest/json-book/', views.view_json_quest_book, name='view_json_quest_book'),
+
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+import json
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.urls import reverse
